0c756d0eb99fa4835cdb0ac95cc51f69cc5e30ab
mgkim <mnii5625@gmail.com>
2025-11-27 01:29:36 +0900

    install & apply eslint-plugin-better-tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`
+          ${geistSans.variable}
+          ${geistMono.variable}
+          antialiased
+        `}
       >
         <ThemeProvider enableSystem={true} defaultTheme="system">
           {children}
