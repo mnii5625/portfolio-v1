7f783c02514a441b13c9991375c9b9e0a64725ce
mgkim <mnii5625@gmail.com>
2025-11-28 11:11:29 +0900

    install & apply @stylistic/eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import eslintParserTypeScript from "@typescript-eslint/parser";
-import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintParserTypeScript from '@typescript-eslint/parser';
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
+import stylistic from '@stylistic/eslint-plugin';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   {
-    files: ["**/*.{ts,tsx,cts,mts}"],
+    files: ['**/*.{ts,tsx,cts,mts}'],
     languageOptions: {
       parser: eslintParserTypeScript,
       parserOptions: {
@@ -17,7 +18,7 @@ const eslintConfig = defineConfig([
     }
   },
   {
-    files: ["**/*.{jsx,tsx}"],
+    files: ['**/*.{jsx,tsx}'],
     languageOptions: {
       parserOptions: {
         ecmaFeatures: {
@@ -26,31 +27,40 @@ const eslintConfig = defineConfig([
       }
     },
     plugins: {
-      "better-tailwindcss": eslintPluginBetterTailwindcss
+      'better-tailwindcss': eslintPluginBetterTailwindcss
     },
     rules: {
       // enable all recommended rules to report a warning
-      ...eslintPluginBetterTailwindcss.configs["recommended-warn"].rules,
+      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
       // enable all recommended rules to report an error
-      ...eslintPluginBetterTailwindcss.configs["recommended-error"].rules,
+      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
 
       // or configure rules individually
-      "better-tailwindcss/enforce-consistent-line-wrapping": ["warn", { printWidth: 100 }]
+      'better-tailwindcss/enforce-consistent-line-wrapping': ['warn', { printWidth: 100 }]
     },
     settings: {
-      "better-tailwindcss": {
+      'better-tailwindcss': {
         // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
-        entryPoint: "app/globals.css",
+        entryPoint: 'app/globals.css',
       }
     }
+  },
+  {
+    plugins: { '@stylistic': stylistic },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+    },
   },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
