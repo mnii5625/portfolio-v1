64d639af0b0e13790a7f95e99d0c90b3a8fe24ee
mgkim <mnii5625@gmail.com>
2025-12-01 15:14:19 +0900

    adjust `ParticleClient` z-index and disable particle hover interactions

--- app/_components/ui/ParticleClient.tsx
+++ app/_components/ui/ParticleClient.tsx
@@ -4,7 +4,7 @@ import Particles from '@/app/_components/ui/Particles';
 
 export default function ParticleClient() {
   return (
-    <div className="fixed inset-0 z-50 h-screen w-screen">
+    <div className="pointer-events-none fixed inset-0 -z-10 h-screen w-screen">
       <div className="relative h-full w-full">
         <Particles
           particleColors={['#ffffff', '#ffffff', '#ffffff']}
@@ -12,7 +12,7 @@ export default function ParticleClient() {
           particleSpread={10}
           speed={0.1}
           particleBaseSize={40}
-          moveParticlesOnHover={true}
+          moveParticlesOnHover={false}
           alphaParticles={false}
           disableRotation={true}
           className="pointer-events-auto"
